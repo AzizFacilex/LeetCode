5e5a408f3cd5db086e36c2262d859a5253172fe1
aziz <khallediaziz@gmail.com>
2024-02-17 00:09:37 +0100

    optimize

--- 6.Day/1662. Check If Two String Arrays are Equivalent.ts
+++ 6.Day/1662. Check If Two String Arrays are Equivalent.ts
@@ -1,3 +1,3 @@
 function arrayStringsAreEqual(word1: string[], word2: string[]): boolean {
-    return word1.reduce((acc,curr)=> acc+curr,'') === word2.reduce((acc,curr)=> acc+curr,'')
+    return word1.join('') === word2.join('')
 };
