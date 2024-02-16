c458d5f9074504a2732620b9157eaaef2b3fd784
aziz <khallediaziz@gmail.com>
2024-02-17 00:22:08 +0100

    edit

--- 6.Day/2652. Sum Multiples.ts
+++ 6.Day/2652. Sum Multiples.ts
@@ -1,6 +1,6 @@
 function sumOfMultiples(n: number): number {
     let result:number=0
-    for (let index = 1; index <= n; index++) {
+    for (let index = 3; index <= n; index++) {
         if (index%3===0 || index%5===0 || index%7===0)
             result+=index
     }
