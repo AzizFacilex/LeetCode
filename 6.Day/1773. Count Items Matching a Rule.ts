1b5ee7ccd6094f1b137798fb3908d8aa4276edb2
aziz <khallediaziz@gmail.com>
2024-02-17 00:48:48 +0100

    optimize

--- 6.Day/1773. Count Items Matching a Rule.ts
+++ 6.Day/1773. Count Items Matching a Rule.ts
@@ -1,20 +1,15 @@
 function countMatches(items: string[][], ruleKey: string, ruleValue: string): number {
-    let result:number=0
-    for (let index = 0; index < items.length; index++) {
-        const element = items[index];
-        if (element.some((x,i)=>{
-            if (x===ruleValue) {
-                if (i===0 && ruleKey==='type') {
-                    return true
-                } else if (i===1 && ruleKey==='color'){
-                    return true
-                } else if (i===2 && ruleKey==='name'){
-                    return true
-                }
-            }
-        }))
-            result++   
-              
-    }
-    return result
+    let count = 0
+    items.forEach((item) => {
+       if (ruleKey === 'type' && item[0] === ruleValue) {
+            count++    
+       }
+       if (ruleKey === 'color' && item[1] === ruleValue) {
+            count++
+       }
+       if (ruleKey === 'name' && item[2] === ruleValue) {
+            count++
+       }
+    })
+    return count
 };
